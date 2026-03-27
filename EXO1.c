1e608b386f0d44e0283b9597e4370bbcb66c47c3
ednowflow-art <ednowflow@gmail.com>
2026-03-27 05:41:07 +0100

    exoc

---
+++ EXO1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main(){
+    int i,n,j,nbrdiv,somdiv,nbrdivp,val,somspecial, nbrhypocrite,nbrdivpre;
+    do{
+        printf("saisir un entier");
+        scanf("%d",&n);
+        nbrdiv=0;
+        for(i=1;i<=n;i++){
+            if(n%i==0){
+                nbrdiv++;
+            }
+        }
+
+    }while(n<0 && nbrdiv!=0);
+     nbrhypocrite=0;
+     somspecial=0;
+    for(i=1;i<=n;i++){
+        printf("saisir une valeur");
+        scanf("%d",&val);
+    nbrdivp=0;
+    somdiv=0;
+    nbrdivpre=0;
+     for(j=1;j<=val;j++){
+        if(val%j==0){
+            nbrdiv=nbrdiv+1;
+            somdiv=somdiv+j;
+            nbrdivpre=nbrdivpre+1;
+        }
+        }
+    if(nbrdiv>2 && somdiv==val*2 && val%2==0 ){
+      somspecial=somspecial+val;
+    }
+    if(nbrdivpre==2 && val%2==0){
+        nbrhypocrite=nbrhypocrite+1;
+    }
+    }
+
+    return 0;
+}
